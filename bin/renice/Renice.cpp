ac88c7f0bfb72e719d396196397ce10d3eff3a5c
Aseward1005 <anthony.seward1005@gmail.com>
2023-04-04 16:47:56 -0700

    new file:   bin/renice/Renice.cpp 	new file:   bin/renice/Renice.h

---
+++ bin/renice/Renice.cpp
@@ -0,0 +1,1 @@
+#include "Renice.h"
---
+++ bin/renice/Renice.h
@@ -0,0 +1,24 @@
+#ifndef __BIN_RENICE_RENICE_H
+#define __BIN_RENICE_RENICE_H
+
+#include <POSIXApplication.h>
+
+class Renice : public POSIXApplication
+{
+    public:
+
+    /**
+     *
+     * @param argc argument count
+     * @param argv argument values
+     */
+    Renice(int argc, char* argv);
+
+    /* 
+     * Execute the application.
+     * @return Result code
+     */
+    virtual Result exec();
+}
+
+#endif
